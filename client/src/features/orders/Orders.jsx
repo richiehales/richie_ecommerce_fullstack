d1b844cd877116d0692726ad22b7603c233dc29d
Richie <richiecontrolsystems@outlook.com>
2024-01-15 08:46:29 +0000

    Orders.jsx styling updated

--- client/src/features/orders/Orders.jsx
+++ client/src/features/orders/Orders.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import { fetchOrders } from './getOrders'
+import { fetchOrders } from './getOrders';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -20,19 +23,24 @@ export default function Review() {
 
 
   return (
-    <div style={{ margin: '0 auto', maxWidth: '600px' }}>
-      <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
-        {`Order History for ${currentUser.first_name} ${currentUser.last_name}`}
-      </Typography>
-      <List disablePadding>
-        {orders.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.description} />
-            <Typography variant="body2">{product.price}</Typography>
-          </ListItem>
-        ))}
-      </List>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>          
+          <Typography variant="h6" gutterBottom sx={{ textAlign: 'center' }}>
+            {`Order History for ${currentUser.first_name} ${currentUser.last_name}`}
+          </Typography>
+          <List disablePadding>
+            {orders.map((product) => (
+              <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+                <ListItemText primary={product.name} secondary={product.description} />
+                <Typography variant="body2">{product.price}</Typography>
+              </ListItem>
+            ))}
+          </List>          
+        </Paper>
+      </Container>
+    </React.Fragment>
   );
 }
 
