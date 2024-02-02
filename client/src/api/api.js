0deabb47e8637a7e4b9023581672c17e460e9993
richard hales <dellxps@controlsystems.local>
2024-02-02 13:05:27 +0000

    Office Backup = 02/02/24 - 13:05

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -261,25 +261,6 @@ export const processPaymentById = async (cardNumber, cardDate, cvc, userId) => {
 };
 
 
-/*
-// Get order by user_id
-// http://localhost:3000/order/1
-const fetchOrdersByIdAPI = 'http://localhost:3000/order/'
-export const fetchOrdersById = async (userId) => {
-  try {
-    const response = await fetch(`${fetchOrdersByIdAPI}${userId}`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok.');
-    }
-    const orders = await response.json(); 
-    return orders;
-  } catch (error) {
-    // Handle errors, log, or throw as necessary  
-    throw error;
-  }
-}
-*/
-
 // Get order by user_id - jwt
 const fetchOrdersByIdAPI = 'http://localhost:3000/order/';
 
