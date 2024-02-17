646e609076be18a56b141630b260394c52191c52
Richie <richiecontrolsystems@outlook.com>
2024-02-17 12:38:30 +0000

    Search Bar width adjusted

--- client/src/features/header/Header.jsx
+++ client/src/features/header/Header.jsx
@@ -45,7 +45,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(1)})`,
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
