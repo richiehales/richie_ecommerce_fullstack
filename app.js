f610d45a8ee34b5d4722cc515296ff9ae37d8351
richard hales <dellxps@controlsystems.local>
2024-02-20 12:22:23 +0000

    cosole.log added to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+console.log('app.js process.env', process.env.NODE_ENV)
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
